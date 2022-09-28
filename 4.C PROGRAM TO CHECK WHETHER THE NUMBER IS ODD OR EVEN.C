e19f903e1d62b6c6713b4f413e541c313810cd4c
Abishake777 <113846954+Abishake777@users.noreply.github.com>
2022-09-28 13:25:21 +0530

    Create 4.C PROGRAM TO CHECK WHETHER THE NUMBER IS ODD OR EVEN.C

---
+++ 4.C PROGRAM TO CHECK WHETHER THE NUMBER IS ODD OR EVEN.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a number to check for even / odd:");
+    scanf("%d",&num);
+    if(num%2 == 0)
+        printf("%d is even",num);
+    else
+        printf("%d is odd",num);
+    return 0;
+}
