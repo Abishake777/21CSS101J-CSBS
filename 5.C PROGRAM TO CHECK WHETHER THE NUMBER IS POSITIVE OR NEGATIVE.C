5ad773bb7e5fc742404eab8fb93a91da9334fc22
Abishake777 <113846954+Abishake777@users.noreply.github.com>
2022-09-28 13:32:57 +0530

    Create 5.C PROGRAM TO CHECK WHETHER THE NUMBER IS POSITIVE OR NEGATIVE.C

---
+++ 5.C PROGRAM TO CHECK WHETHER THE NUMBER IS POSITIVE OR NEGATIVE.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int num;
+ 
+    printf("Enter a number: \n");
+    scanf("%d", &num);
+    if (num > 0)
+        printf("%d is a positive number \n", num);
+    else if (num < 0)
+        printf("%d is a negative number \n", num);
+    else
+        printf("0 is neither positive nor negative");
+        
+    return 0;    
+}
