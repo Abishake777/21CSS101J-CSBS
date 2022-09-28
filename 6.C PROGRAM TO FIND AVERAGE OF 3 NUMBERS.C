8d0d9ababc2e390094de8dd2ba1de6874ad37581
Abishake777 <113846954+Abishake777@users.noreply.github.com>
2022-09-28 13:43:45 +0530

    Create 6.C PROGRAM TO FIND AVERAGE OF 3 NUMBERS.C

---
+++ 6.C PROGRAM TO FIND AVERAGE OF 3 NUMBERS.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+  
+int main()
+  
+{
+  
+  float a,b,c,avg;
+  
+  printf("Please enter 3 numbers ");
+  
+  scanf("%f%f%f", &a, &b, &c);
+  
+  avg=(a+b+c)/3;
+  
+  
+  printf("\nAverage is  %f",avg);
+  
+  return 0;
+  
+}
