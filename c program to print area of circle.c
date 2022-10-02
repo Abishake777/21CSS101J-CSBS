38e2d727d542bc6d0b326231cd45ba30de65b4c0
Abishake777 <113846954+Abishake777@users.noreply.github.com>
2022-10-02 10:46:42 +0530

    Create c program to print area of circle.c

---
+++ c program to print area of circle.c
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+int main() 
+{ 
+int r; 
+float PI = 3.14, area; 
+printf("Enter radius of circle: "); 
+scanf("%d", &r); 
+area = PI * r * r; 
+printf("\nArea of circle : %f ", area); 
+return 0; 
+} 
