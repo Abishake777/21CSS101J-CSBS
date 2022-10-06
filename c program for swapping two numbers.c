2832fcc7801c34e43a5a9a3e92dce17c5612747d
Abishake777 <113846954+Abishake777@users.noreply.github.com>
2022-10-06 08:10:08 +0530

    Update c program for swapping two numbers.c

--- c program for swapping two numbers.c
+++ c program for swapping two numbers.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main() 
 {
-  int first, second, temp;
+  int first, second, num;
   printf("Enter the first number: ");
   scanf("%d", &first);
   printf("Enter the second number: ");
   scanf("%d", &second);
 
-  temp = first;
+  num = first;
   first = second;
-  second = temp;
+  second = num;
   
   printf("\nFirst number after swapping is = %d", first);
   printf("\nSecond number after swapping is = %d", second);
