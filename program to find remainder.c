2259c4cef5aced34053c987f040ddfc812563a46
Abishake777 <113846954+Abishake777@users.noreply.github.com>
2022-10-12 16:37:09 +0530

    Update program to find remainder.c

--- program to find remainder.c
+++ program to find remainder.c
@@ -3,5 +3,5 @@ main()
 {
   int a=10, b=5;
   printf("registor number-RA2211042010054\n") 
-  printf("The remainder we get when a/b is = %d",a%b)
+  printf("The remainder we get when a/b is = %d",a%b);
  }
