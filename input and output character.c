b241ab1e0b0eb1ea0065450060f134e31b8e4399
Abishake777 <113846954+Abishake777@users.noreply.github.com>
2022-10-14 13:17:10 +0530

    Update and rename character input and output.c to  input and output character.c

--- input and output character.c
+++ input and output character.c
@@ -2,6 +2,7 @@
 int main()
 {
     char chr;
+    printf("registor number-RA2211042010054\n");    
     printf("Enter a character: ");
     scanf("%c",&chr);     
     printf("You entered %c.", chr);  
