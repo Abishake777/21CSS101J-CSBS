89c6ec9b2e21cd234b1870ac22ce7dabbdcf1455
Abishake777 <113846954+Abishake777@users.noreply.github.com>
2022-10-14 13:59:49 +0530

    Update pre decrement.c

--- pre decrement.c
+++ pre decrement.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 main()
 {
-int a=3;
-printf("registor number-RA2211042010054\n");	
-printf("The decremented value of a is = %d",--a);
+  int a=3;
+  printf("registor number-RA2211042010054\n");	
+  printf("The decremented value of a is = %d",--a);
 }
