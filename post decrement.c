b36b110e1eb9e639b36627fe01e54a9834b553f4
Abishake777 <113846954+Abishake777@users.noreply.github.com>
2022-10-14 13:49:45 +0530

    Update post decrement.c

--- post decrement.c
+++ post decrement.c
@@ -2,6 +2,7 @@
 main()
 {
   int a=3;
+  printf("registor number-RA2211042010054\n");	 
   printf("The value of a returned after post decrement is = %d",a--);
   printf("\nThe value of a now is = %d",a);
 }
