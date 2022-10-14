2d95d23058b4c796b6794f45a7c954d2ee31085f
Abishake777 <113846954+Abishake777@users.noreply.github.com>
2022-10-14 13:58:53 +0530

    Update print characters.c

--- print characters.c
+++ print characters.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main()
 {
-    char chr = 'a';    
+    char chr = 'a';
+    printf("registor number-RA2211042010054\n");	 
     printf("character = %c", chr);  
     return 0;
 } 
