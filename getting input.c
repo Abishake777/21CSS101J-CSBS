065898e8e98a7bd66aaf880fd9c9eaafac71eb64
Abishake777 <113846954+Abishake777@users.noreply.github.com>
2022-10-14 13:20:39 +0530

    Update getting input.c

--- getting input.c
+++ getting input.c
@@ -2,6 +2,7 @@
 int main()
 {
   int input_number;
+  printf("registor number-RA2211042010054\n"); 
   printf("Enter a number:");
   scanf("%d", &input_number);
   printf("The number we got as input is = %d",input_number);
