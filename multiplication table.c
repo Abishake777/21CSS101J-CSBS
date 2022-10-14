2e878deb6d84ae4d10dff5f4e1d841c4439a1653
Abishake777 <113846954+Abishake777@users.noreply.github.com>
2022-10-14 13:21:52 +0530

    Update multiplication table.c

--- multiplication table.c
+++ multiplication table.c
@@ -5,7 +5,6 @@ int main() {
   printf("Enter an integer: ");
   scanf("%d", &n);
   for (i = 1; i <= 10; ++i) 
-  printf("%d * %d = %d \n", n, i, n * i);
-  
+  printf("%d * %d = %d \n", n, i, n * i); 
   return 0;
 }
