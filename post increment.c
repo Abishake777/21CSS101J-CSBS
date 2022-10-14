7d454c5ba9307db4707d7ec23b098077b3c27cea
Abishake777 <113846954+Abishake777@users.noreply.github.com>
2022-10-14 14:01:04 +0530

    Update post increment.c

--- post increment.c
+++ post increment.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 main()
 {
-int a=2;
-printf("registor number-RA2211042010054\n");	 
-printf("The value returned after post increment of a is = %d",a++);
-printf("\nThe value of a now is = %d",a);
+  int a=2;
+  printf("registor number-RA2211042010054\n");	 
+  printf("The value returned after post increment of a is = %d",a++);
+  printf("\nThe value of a now is = %d",a);
 }
