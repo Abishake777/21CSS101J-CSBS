ddd06f312aeec251956fb7f608b8a8fda089825b
Abishake777 <113846954+Abishake777@users.noreply.github.com>
2022-10-14 13:25:35 +0530

    Update operator size of operator (float+float).c

--- operator size of operator (float+float).c
+++ operator size of operator (float+float).c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main()
 {
- printf("Size of the numerical expression (float and float) = %ld\n", sizeof(7.2+6.835));
+ printf("registor number-RA2211042010054\n"); 
+ printf("Size of the numerical expression (float and float) = %ld\n", sizeof(7.2+6.812));
  return 0;
 }
