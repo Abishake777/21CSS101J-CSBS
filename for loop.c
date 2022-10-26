bbe7a05fe9d5c3fadf2a5d714e0c0c82609bc350
Abishake777 <113846954+Abishake777@users.noreply.github.com>
2022-10-26 10:06:35 +0530

    Create for loop.c

---
+++ for loop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){ 
+  printf("Regester  Number :RA2211042010054\n");
+int sum= 0;int n;
+for(n = 1;n<10;n= n+2)
+{
+    sum = sum+ n*n;
+}
+printf("sum = %d\n", sum);
+}
